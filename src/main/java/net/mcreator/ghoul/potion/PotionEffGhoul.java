0fc63aed954e62af7e23418bab5b6a50effbd140
Smile1818 <dfyzsmile@gmail.com>
2025-02-06 13:37:39 +0500

    Add files via upload

---
+++ src/main/java/net/mcreator/ghoul/potion/PotionEffGhoul.java
@@ -0,0 +1,101 @@
+
+package net.mcreator.ghoul.potion;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.Potion;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.ghoul.procedure.ProcedureEfGhoul;
+import net.mcreator.ghoul.item.ItemKakuneNul;
+import net.mcreator.ghoul.ElementsGhoul;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@ElementsGhoul.ModElement.Tag
+public class PotionEffGhoul extends ElementsGhoul.ModElement {
+	@GameRegistry.ObjectHolder("ghoul:effghoul")
+	public static final Potion potion = null;
+	public PotionEffGhoul(ElementsGhoul instance) {
+		super(instance, 57);
+	}
+
+	@Override
+	public void initElements() {
+		elements.potions.add(() -> new PotionCustom());
+	}
+	public static class PotionCustom extends Potion {
+		private final ResourceLocation potionIcon;
+		public PotionCustom() {
+			super(false, -1);
+			setRegistryName("effghoul");
+			setPotionName("effect.effghoul");
+			potionIcon = new ResourceLocation("ghoul:textures/mob_effect/effghoul.png");
+		}
+
+		@Override
+		public boolean isInstant() {
+			return false;
+		}
+
+		@Override
+		public List<ItemStack> getCurativeItems() {
+			List<ItemStack> ret = new ArrayList<>();
+			ret.add(new ItemStack(ItemKakuneNul.block, (int) (1)));
+			return ret;
+		}
+
+		@Override
+		public boolean shouldRenderInvText(PotionEffect effect) {
+			return true;
+		}
+
+		@Override
+		public boolean shouldRenderHUD(PotionEffect effect) {
+			return true;
+		}
+
+		@Override
+		public void performEffect(EntityLivingBase entity, int amplifier) {
+			World world = entity.world;
+			int x = (int) entity.posX;
+			int y = (int) entity.posY;
+			int z = (int) entity.posZ;
+			{
+				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+				$_dependencies.put("entity", entity);
+				ProcedureEfGhoul.executeProcedure($_dependencies);
+			}
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc) {
+			if (mc.currentScreen != null) {
+				mc.getTextureManager().bindTexture(potionIcon);
+				Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0, 0, 18, 18, 18, 18);
+			}
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public void renderHUDEffect(int x, int y, PotionEffect effect, Minecraft mc, float alpha) {
+			mc.getTextureManager().bindTexture(potionIcon);
+			Gui.drawModalRectWithCustomSizedTexture(x + 3, y + 3, 0, 0, 18, 18, 18, 18);
+		}
+
+		@Override
+		public boolean isReady(int duration, int amplifier) {
+			return true;
+		}
+	}
+}
